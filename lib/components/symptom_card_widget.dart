8d9e67dda7c3f9d38b8dc3b0dcc667760bf1194f
ihassanjavaid <great.maliks@gmail.com>
2020-05-07 00:25:35 +0500

    Update symptom_card_widget.dart

--- lib/components/symptom_card_widget.dart
+++ lib/components/symptom_card_widget.dart
@@ -13,40 +13,59 @@ class SymptomCard extends StatefulWidget {
 }
 
 class _SymptomCardState extends State<SymptomCard> {
+  double _height = 100;
+  double _width = 140;
+  Color _color = Colors.white;
+
+  _animateContainer(){
+    setState(() {
+      _height = _height == 100 ? 120 : 100;
+      _width = _width == 140 ? 160 : 140;
+      _color = _color == Colors.white ? kInfectedColor : Colors.white;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: ,
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: Colors.white,
-          boxShadow: [
-            widget.isActive
-                ? BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 20,
-                    color: kActiveShadowColor,
-                  )
-                : BoxShadow(
-                    offset: Offset(0, 3),
-                    blurRadius: 6,
-                    color: kShadowColor,
-                  ),
-          ]),
-      child: Column(
-        children: <Widget>[
-          Image.asset(
-            widget.image,
-            height: 90,
-          ),
-          Text(
-            widget.title,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        _animateContainer();
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 200),
+        height: _height,
+        width: _width,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: _color,
+            boxShadow: [
+              widget.isActive
+                  ? BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 20,
+                      color: kActiveShadowColor,
+                    )
+                  : BoxShadow(
+                      offset: Offset(0, 3),
+                      blurRadius: 6,
+                      color: kShadowColor,
+                    ),
+            ]),
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              widget.image,
+              height: 90,
             ),
-          ),
-        ],
+            Text(
+              widget.title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
