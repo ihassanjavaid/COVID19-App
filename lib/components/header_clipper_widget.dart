4335440eacd9b8abea5e887b5b769eb191c92eb6
ihassanjavaid <great.maliks@gmail.com>
2020-05-06 20:59:05 +0500

    GUI_update clipper fixed

---
+++ lib/components/header_clipper_widget.dart
@@ -0,0 +1,73 @@
+import 'package:covid19/components/custom_clipper.dart';
+import 'package:covid19/utilities/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+class Header extends StatelessWidget {
+  final String image;
+  final String textTop;
+  final String textBottom;
+
+  const Header({
+    Key key,
+    this.image,
+    this.textTop,
+    this.textBottom,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: CustomCurveClipper(),
+      child: Container(
+        padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+        height: 240,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF3383CD),
+              Color(0xFF11249f),
+            ],
+          ),
+          /*image: DecorationImage(
+            image: AssetImage('assets/images/virus.png'),
+          ),*/
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Align(
+                alignment: Alignment.topRight,
+                child: SvgPicture.asset('assets/icons/menu.svg')),
+            Expanded(
+              child: Stack(
+                children: <Widget>[
+                  SvgPicture.asset(
+                   image,
+                    width: 130,
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.topCenter,
+                  ),
+                  Positioned(
+                    top: 20,
+                    left: 130,
+                    child: Text(
+                      '$textTop \n$textBottom',
+                      style:
+                      kHeadingTextStyle.copyWith(color: Colors.white),
+                    ),
+                  ),
+                  Container(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:covid19/case_counter_widget.dart';
-import 'package:covid19/components/custom_clipper.dart';
+import 'package:covid19/components/header_clipper_widget.dart';
 import 'package:covid19/utilities/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,59 +11,10 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          ClipPath(
-            clipper: CustomCurveClipper(),
-            child: Container(
-              padding: EdgeInsets.only(left: 40, top: 50, right: 20),
-              height: 350,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Color(0xFF3383CD),
-                    Color(0xFF11249f),
-                  ],
-                ),
-                image: DecorationImage(
-                  image: AssetImage('assets/images/virus.png'),
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Align(
-                      alignment: Alignment.topRight,
-                      child: SvgPicture.asset('assets/icons/menu.svg')),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Expanded(
-                    child: Stack(
-                      children: <Widget>[
-                        SvgPicture.asset(
-                          'assets/icons/Drcorona.svg',
-                          width: 230,
-                          fit: BoxFit.fitWidth,
-                          alignment: Alignment.topCenter,
-                        ),
-                        Positioned(
-                          top: 20,
-                          left: 150,
-                          child: Text(
-                            'All you need \nis stay at home.',
-                            style:
-                                kHeadingTextStyle.copyWith(color: Colors.white),
-                          ),
-                        ),
-                        Container(),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          Header(
+            image: 'assets/icons/Drcorona.svg',
+            textTop: "All you need",
+            textBottom: "is stay at home.",
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20),
@@ -191,7 +142,9 @@ class HomeScreen extends StatelessWidget {
                   ],
                 ),
                 Container(
-                  height: 170,
+                  margin: EdgeInsets.only(top: 20),
+                  padding: EdgeInsets.all(2),
+                  height: 125,
                   width: double.infinity,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
@@ -204,7 +157,9 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ]
                   ),
-                  child: Image.asset('assets/images/map.png'),
+                  child: Image.asset('assets/images/map.png',
+                  fit: BoxFit.contain,
+                  ),
                 ),
               ],
             ),
