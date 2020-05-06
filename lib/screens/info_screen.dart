1e43e8d98e1cb37d3646f48c8df884bb1cd45ee8
ihassanjavaid <great.maliks@gmail.com>
2020-05-06 23:50:47 +0500

    update

--- lib/screens/info_screen.dart
+++ lib/screens/info_screen.dart
@@ -11,73 +11,82 @@ class InfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Header(
-              image: 'assets/icons/coronadr.svg',
-              textTop: 'Get to know',
-              textBottom: 'About Covid-19',
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    'Symptoms',
-                    style: kTitleTextstyle,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      SymptomCard(
-                        image: 'assets/images/headache.png',
-                        title: 'Headache',
-                        isActive: true,
+      body: Column(
+        children: <Widget>[
+          Header(
+            image: 'assets/icons/coronadr.svg',
+            textTop: 'Get to know',
+            textBottom: 'About Covid-19',
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Symptoms',
+                      style: kTitleTextstyle,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Flexible(
+                      child: ListView(
+                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          SymptomCard(
+                            image: 'assets/images/headache.png',
+                            title: 'Headache',
+                            isActive: true,
+                          ),
+                          SymptomCard(
+                            image: 'assets/images/caugh.png',
+                            title: 'Cough',
+                          ),
+                          SymptomCard(
+                            image: 'assets/images/fever.png',
+                            title: 'Fever',
+                          ),
+                          SymptomCard(
+                            image: 'assets/images/headache.png',
+                            title: 'Lethargy',
+                          ),
+                        ],
                       ),
-                      SymptomCard(
-                        image: 'assets/images/caugh.png',
-                        title: 'Cough',
-                      ),
-                      SymptomCard(
-                        image: 'assets/images/fever.png',
-                        title: 'Fever',
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Prevention',
-                    style: kTitleTextstyle,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  PreventionCard(
-                    text:
-                        'Since the start of the Coronavirus outbreak some places have fully embraced wearing facemasks',
-                    image: 'assets/images/wear_mask.png',
-                    title: 'Wear face mask',
-                  ),
-                  PreventionCard(
-                    text:
-                        'Since the start of0 the Coronavirus outbreak some places have fully embraced washing hands',
-                    image: 'assets/images/wash_hands.png',
-                    title: 'Wash your hands',
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Prevention',
+                      style: kTitleTextstyle,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    PreventionCard(
+                      text:
+                          'Since the start of the Coronavirus outbreak some places have fully embraced wearing facemasks',
+                      image: 'assets/images/wear_mask.png',
+                      title: 'Wear face mask',
+                    ),
+                    PreventionCard(
+                      text:
+                          'Since the start of0 the Coronavirus outbreak some places have fully embraced washing hands',
+                      image: 'assets/images/wash_hands.png',
+                      title: 'Wash your hands',
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
